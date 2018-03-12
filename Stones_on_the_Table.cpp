9610f677d8104afb93de60b1ddd57a544a6aee85
mintakoh <omt1537@naver.com>
2018-03-12 23:36:51 +0900

    266A Stones on the Table

---
+++ Stones_on_the_Table.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    string s;
+    int count = 0;
+
+    cin >> s;
+
+    for(int i = 0; i < s.length() - 1; i++) {
+        if(s[i] == s[i+1]) {
+            count++;
+        }
+    }
+    
+    cout << count << endl;
+
+    return 0;
+}
